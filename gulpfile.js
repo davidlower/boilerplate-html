72ff0f4a80bb87e65bdc080e6683c9d887df1283
David <davidlower4@gmail.com>
2019-07-01 19:23:55 +0100

    Fresh change to gulp file and also to the SCSS structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,137 +2,134 @@
 
 const gulp = require('gulp');
 // MINIFY PLUGINS
-const uglify = require('gulp-uglify');
+const uglify = require('gulp-uglify-es');
 const cleanCSS = require('gulp-clean-css');
 const htmlmin = require('gulp-htmlmin');
 const purge = require('gulp-css-purge');
-const groupmq = require('gulp-group-css-media-queries');
 // CONCAT PLUGIN
 const concat = require('gulp-concat');
+// STRIP OUT JS FILES
+const stripDebug = require('gulp-strip-debug');
 // IMAGE COMPRESSION PLUGINS
 const imagemin = require('gulp-imagemin');
-const imageminOptipng = require('imagemin-optipng');
+const imageminPngquant = require('imagemin-pngquant');
 const imageminSvgo = require('imagemin-svgo');
 const imageminMozjpeg = require('imagemin-mozjpeg');
+const imageminJpegtran = require('imagemin-jpegtran');
+const imageminWebp = require('imagemin-webp');
 const newer = require('gulp-newer');
+const rename = require("gulp-rename");
 // BROWSER-SYNC PLUGIN
 const browserSync = require('browser-sync').create();
 // CSS & AUTOPREFIXER PLUGINS
 const autoprefixer = require('gulp-autoprefixer');
 const sass = require('gulp-sass');
+const globbing = require('gulp-css-globbing');
 // SOURCEMAPS
 const sourcemaps = require('gulp-sourcemaps');
 
-//CSS
+//CSS BUILD
 function css(done) {
-   gulp.src('src/**/*.scss', 'src/scss/**/*.css')
+   gulp.src('src/scss/**/*.scss')
       .pipe(sourcemaps.init())
+      .pipe(globbing({
+         extensions: ['.scss']
+      }))
       .pipe(sass.sync().on('error', sass.logError))
-      .pipe(concat('main.css'))
       .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest('production'));
+      .pipe(gulp.dest('production/css'))
+      .pipe(browserSync.stream());
    done();
 }
 
 // CSS BUILD - AUTOPREFIXER, MINIFICATION, PURGING, MEDIA-QUERIES
 function cssBuild(done) {
-   gulp.src('production/*.css')
-      .pipe(groupmq())
+   gulp.src('production/css/**/*.css')
       .pipe(purge({
          trim: true,
          shorten: true,
          verbose: true
       }))
       .pipe(autoprefixer({
-         browsers: ['last 2 versions'],
          cascade: false
       }))
       .pipe(cleanCSS({
          compatibility: 'ie8'
       }))
-      .pipe(gulp.dest('production'));
-   done();
-}
-
-// HTML
-function html(done) {
-   gulp.src('src/*.html')
-      .pipe(gulp.dest('production'));
+      .pipe(gulp.dest('production/css'));
    done();
 }
 
 // HTML BUILD - MINIFY HTML
 function htmlBuild(done) {
-   gulp.src('production/*.html')
+   gulp.src('./*.html')
       .pipe(htmlmin({
          collapseWhitespace: true
       }))
-      .pipe(gulp.dest('production'));
+      .pipe(gulp.dest('./'));
    done();
 }
 
 // JAVASCRIPT
 function js(done) {
-   gulp.src('src/**/*.js')
-      .pipe(concat('main.js'))
-      .pipe(gulp.dest('production'));
+   gulp.src('src/js/**/*.js')
+      .pipe(concat('bundle.js'))
+      .pipe(gulp.dest('production/js'));
    done();
 }
 
 // JAVASCRIPT BUILD - MINIFY JS
 function jsBuild(done) {
-   gulp.src('production/*.js')
+   gulp.src('production/js/**/*.js')
+      .pipe(stripDebug())
       .pipe(uglify())
-      .pipe(gulp.dest('production'));
-   done();
-}
-
-// LAUNCH THIS AT THE BEGINNING OF ANY PROJECT - GRABS ALL ASSETS FROM NODE MODULES
-// If sections are commented out it means they are not needed for this project
-function launchProject(done) {
-   // LITY - LIGHTBOX MODULE FOR JQUERY
-   gulp.src('node_modules/lity/dist/lity.min.js')
-      .pipe(gulp.dest('src/assets/js'));
-   gulp.src('node_modules/lity/dist/lity.min.css')
-      .pipe(gulp.dest('src/assets/sass'));
-
-   // JQUERY
-   gulp.src('node_modules/jquery/dist/jquery.min.js')
-      .pipe(gulp.dest('src/assets/js'));
-
-   // SIMPLE GRID - GRID SYSTEM LIKE BOOTSTRAP
-   gulp.src('node_modules/simplegrid/simple-grid.scss')
-      .pipe(gulp.dest('src/assets/sass'));
-
-   // SLICK CAROUSEL - SLIDER MODULE FOR JQUERY
-   gulp.src('node_modules/slick-carousel/slick/slick.min.js')
-      .pipe(gulp.dest('src/assets/js'));
-   gulp.src('node_modules/slick-carousel/slick/slick.scss')
-      .pipe(gulp.dest('src/assets/sass'));
+      .pipe(gulp.dest('production/js'));
    done();
 }
 
 //IMAGE COMPRESSION
-function imgCompression(done) {
+function imgCompress(done) {
    gulp.src('src/img/**/*')
       .pipe(newer('production/img'))
       .pipe(imagemin([
          //png
-         imageminOptipng({
-            optimizationLevel: 5
-         }), // 0-7 low-high.
+         imageminPngquant({
+            speed: 1,
+            Quality: '70-90',
+            floyd: 1
+         }),
+         //jpg
+         imageminJpegtran({
+            progressive: true
+         }),
+         imageminMozjpeg({
+            quality: 78
+         }),
          //svg
          imageminSvgo({
             plugins: [{
                removeViewBox: false
             }]
-         }),
-         //jpg
-         imageminMozjpeg({
-            quality: 79
-         }), // highest quality 100
+         })
       ], {
          verbose: true
+      }))
+      .pipe(gulp.dest('production/img'));
+   done();
+}
+
+function imgWebp(done) {
+   gulp.src('src/img/**/*.{jpg,png}')
+      .pipe(newer('production/img'))
+      .pipe(imagemin([
+         //webp
+         imageminWebp({
+            method: 6,
+            quality: 50
+         })
+      ]))
+      .pipe(rename({
+         extname: '.webp'
       }))
       .pipe(gulp.dest('production/img'));
    done();
@@ -144,7 +141,7 @@ function bs(done) {
       open: false,
       port: 4000,
       server: {
-         baseDir: 'production'
+         baseDir: './'
       }
    });
    done();
@@ -158,11 +155,10 @@ function browserSyncReload(done) {
 function watchFiles(done) {
    gulp.watch('src/**/*.scss', css);
    gulp.watch('src/**/*.js', js);
-   gulp.watch('src/img/**/*', imgCompression);
-   gulp.watch('src/*.html', html);
+   gulp.watch('src/img/**/*', imgCompress);
    gulp.watch(
       [
-         'src/**/*'
+         './**/*.html'
       ],
       browserSyncReload);
    done();
@@ -170,10 +166,9 @@ function watchFiles(done) {
 
 gulp.task('css', css);
 gulp.task('js', js);
-gulp.task('img', imgCompression);
+gulp.task('img', imgCompress);
 gulp.task('cssBuild', cssBuild);
 gulp.task('jsBuild', jsBuild);
 gulp.task('htmlBuild', htmlBuild);
-gulp.task('build', gulp.series(cssBuild, css, jsBuild, js, htmlBuild, html)); // Use this to build your start project & again every time you push to live production
-gulp.task('launch', launchProject); // Use this to fetch your assets or vendors - for example jQuery
-gulp.task('default', gulp.parallel(css, imgCompression, js, html, bs, watchFiles));
+gulp.task('build', gulp.parallel(cssBuild, jsBuild, htmlBuild)); // Use this to build your start project & again every time you push to live production
+gulp.task('default', gulp.parallel(bs, watchFiles));
